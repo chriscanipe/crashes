0ee76ebf3cc2ff0eeaf2e6f38c0aa4aea408e3ae
Chris Canipe <chris.canipe@wsj.com>
2015-03-13 09:59:01 -0500

    updated script.js

--- js/script.js
+++ js/script.js
@@ -52,20 +52,31 @@ d3.csv("js/crashes.csv", function(error, data) {
   });
 
 
-  color.domain(d3.keys(data[0]).filter(function(key) { return key === "Fatalities change" || key === "Crashes change"; }));
+  color.domain(d3.keys(data[0]).filter(function(key) {
+    return key === "Fatalities change" || key === "Crashes change";
+    //return key;
+  }));
 
-  
-
+  /* ****************************************************** */
+  // This is where we set up our data. It's a little complicated, but here's what's basically happening:
+  // "color.domain()"" is an array of two values: ["Fatalities change", "Crashes change"].
+  // We're creating an object for each, and types will be an array of those two objects.
+  // Name is the name of the series: again, "Fatalities change" and "Crashes change".
+  // And values is the date/value series, expressed as an array of objects.
+  /* ****************************************************** */
   var types = color.domain().map(function(name) {
     return {
       name: name,
       values: data.map(function(d) {
-        return {date: d.Yr, change: +d[name]};
+        /* ****************************************************** */
+        // This is where we set the set up each object for the values array.
+        // I added crashes and fatalities to each data point, so we can see them on mouseover.
+        // I also added series name variable, so we can see which line we're looking at.
+        /* ****************************************************** */
+        return {date: d.Yr, change: +d[name], crashes : d.Crashes, fatalities : d.Fatalities, seriesName : name};
       })
     };
   });
-
-  console.log(data, types);
 
   x.domain(d3.extent(data, function(d) { return d.Yr; }));
 
@@ -111,10 +122,25 @@ d3.csv("js/crashes.csv", function(error, data) {
       .text(function(d) { return d.name; });
 
 
- svg.selectAll(".dot")
-      .data(types)
-      .enter()
-      .append("circle")
+
+/* ****************************************************** */
+// Instad of appending dots to the svg, (svg.selectAll(".dot"))...
+// We want to follow the same pattern as above.
+// "var type" is a group created for each line, and the lines are appended to the group.
+// So we actually want to append a series of dots to each line.
+// Therefore, we append to type. (types.selectAll(".dot"))...
+/* ****************************************************** */
+
+type.selectAll(".dot")
+    /* ****************************************************** */
+    //And the data is the values array nested in the data object already attached to type.
+    //This is really tricky, by the way. I'm not sure we covered this specific concept in class.
+    /* ****************************************************** */
+    .data(function(d) {
+      return d.values;
+    })
+    .enter()
+    .append("circle")
     .attr("class", "dot")
     .attr("cx", function(d) {
       return x(d.date);
@@ -123,16 +149,40 @@ d3.csv("js/crashes.csv", function(error, data) {
       return y(d.change);
     })
     .attr("r", 5)
-
     .on("mouseover", function(d) {  
+
       var yearFormat = d3.time.format("%Y");
       var dispDate = yearFormat(d.date);
-      var dispAvg = String(d.AVG.toFixed(3)).replace("0.", ".");
+      
+      /* ****************************************************** */
+      //Create two blank variables. We assign them below in the if/else
+      /* ****************************************************** */
+      var actualLabel;
+      var actualVal;
 
- 
+      /* ****************************************************** */
+      //Using the seriesName variable, we check to see which series we're looking at.
+      //If it's Fatalities, we pull the fatalities total and set the label.
+      //If it's crashes, we do the same for crashes.
+      /* ****************************************************** */
+
+      if (d.seriesName == "Fatalities change") {
+        actualLabel = "Total Fatalities";
+        actualVal = d.fatalities;
+      } else {
+        actualLabel = "Total Crashes";
+        actualVal = d.crashes;
+      }
+
+      //Then I just append it all to the tooltip with some markup I can control in the css.
       $(".tt").html(
         "<div class='date'>"+dispDate+"</div>"+
-        "<div class='val'>"+dispChange+"</div>"
+        "<div class='val'>"+
+          actualLabel+": <b>"+actualVal+"</b>"+
+        "</div>"+
+        "<div class='change'>"+
+          "Change since 2004: <b>"+d.change+"%</b>"+
+        "</div>"
       );
 
       d3.select(this).classed("active", true);
